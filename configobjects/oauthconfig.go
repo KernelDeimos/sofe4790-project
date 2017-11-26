1b105dfcc8f9c54ad8ca14ecf15c177476748a91
Eric Dube <eric.alex.dube@gmail.com>
2017-11-26 14:39:37 -0500

    Add object to parse oauth config (for Twitter)

---
+++ configobjects/oauthconfig.go
@@ -0,0 +1,16 @@
+package configobjects
+
+import "github.com/dghubble/oauth1"
+
+type OAuth struct {
+	ConsumerKey    string `yaml:"consumer-key"`
+	ComsumerSecret string `yaml:"consumer-secret"`
+	Token          string `yaml:"token"`
+	TokenSecret    string `yaml:"token-secret"`
+}
+
+func (conf OAuth) GetOAuth1() (config *oauth1.Config, token *oauth1.Token) {
+	config = oauth1.NewConfig(conf.ConsumerKey, conf.ComsumerSecret)
+	token = oauth1.NewToken(conf.Token, conf.TokenSecret)
+	return
+}
